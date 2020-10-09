ec7265f4abf4e3bd74235c3a6d44069294913260
Alejandro Linan <jandrolinan@gmail.com>
2020-10-09 15:59:01 -0500

    added more border styles

--- src/components/index.js
+++ src/components/index.js
@@ -7,7 +7,37 @@ class Box extends React.Component {
         return(
             <section>
                 <div className='box-container'>
-                    <div className='box'>
+                    <div className='box-one'>
+                        <h1>Hello There!</h1>
+                    </div>
+                </div>
+                <div className='box-container'>
+                    <div className='box-two'>
+                        <h1>Hello There!</h1>
+                    </div>
+                </div>
+                <div className='box-container'>
+                    <div className='box-three'>
+                        <h1>Hello There!</h1>
+                    </div>
+                </div>
+                <div className='box-container'>
+                    <div className='box-four'>
+                        <h1>Hello There!</h1>
+                    </div>
+                </div>
+                <div className='box-container'>
+                    <div className='box-five'>
+                        <h1>Hello There!</h1>
+                    </div>
+                </div>
+                <div className='box-container'>
+                    <div className='box-six'>
+                        <h1>Hello There!</h1>
+                    </div>
+                </div>
+                <div className='box-container'>
+                    <div className='box-seven'>
                         <h1>Hello There!</h1>
                     </div>
                 </div>
